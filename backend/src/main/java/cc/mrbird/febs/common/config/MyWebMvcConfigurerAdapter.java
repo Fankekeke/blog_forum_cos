5933fe9e713d834017c5ec8036503c00dae9457e
Fankekeke <1258846421@qq.com>
2024-09-17 10:07:42 +0800

    fix - 前端初始化

--- backend/src/main/java/cc/mrbird/febs/common/config/MyWebMvcConfigurerAdapter.java
+++ backend/src/main/java/cc/mrbird/febs/common/config/MyWebMvcConfigurerAdapter.java
@@ -9,7 +9,7 @@ public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/imagesWeb/**").addResourceLocations("file:G:/Project_fank/db/");
+        registry.addResourceHandler("/imagesWeb/**").addResourceLocations("file:G:/Project/20240725博客论坛系统/db/");
         WebMvcConfigurer.super.addResourceHandlers(registry);
     }
 }
